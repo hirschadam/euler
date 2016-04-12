e1b1dca242071fc56ffd0b4ab3194fd8f3b7165c
hirschadam <aah1619@rit.edu>
2016-04-12 15:33:11 -0400

    Implemented two solutions. One using my idea and the other using a generator

--- q1.py
+++ q1.py
@@ -1,12 +1,23 @@
 
-
-
 def multiples(n, a, b):
+    total = 0
+    i = 0
+    while(i < n):
+        if(i % a == 0):
+            total += i
+        elif (i % b == 0):
+            total += i
+        i += 1
+    return total
 
+def multiples2():
+    for number in range(1000):
+        if not number % 3 or not number % 5:
+            yield number
+def main():
+    #my solution
+    print (multiples(1000,3,5))
+    #solution using a generator
+    print (sum(multiples2()))
 
-
-
-
-
-
-    multiples(1000, 3, 5)
+main()
